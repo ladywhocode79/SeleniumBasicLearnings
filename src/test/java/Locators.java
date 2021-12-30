c7f64fdfaa7a2ce763d9d0090c705ca1676f8079
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2021-12-30 06:15:27 +0530

    updated code for locators

--- src/test/java/Locators.java
+++ src/test/java/Locators.java
@@ -58,5 +58,105 @@ public class Locators {
         //Regular expression CSS - * is a regular experession matches all elements
         //eg css -> input[type*='pass']=>will match all elements starting with pass static word
 
+        //Regular expression Xpath - *
+        //eg css -> //button[contains(@class,'submit')]=>will match all elements starting with pass static word
+
+        //paremt child xpath with index
+        //eg //div[@class='forgot-pwd-btn-class']//button[1]
+
+        //if there is a single tag then use locator tagnname
+       //    driver.findElement(By.tagName("p"));
+
+        /*// Sibling - Child to parent traverse
+
+        //header/div/button[1]/following-sibling::button[1]
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
+
+*/
+
+
+        /*==code samples from couurse
+        import java.time.Duration;
+
+
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+
+public class Locators {
+
+
+
+public static void main(String[] args) throws InterruptedException {
+
+// TODO Auto-generated method stub
+
+//implicit wait - 2 seconds time out
+
+System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chromedriver");
+
+WebDriver driver = new ChromeDriver();
+
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+
+driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+
+driver.findElement(By.className("signInBtn")).click();
+
+System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
+driver.findElement(By.linkText("Forgot your password?")).click();
+
+Thread.sleep(1000);//
+
+driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+
+driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@rsa.com");
+
+driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+
+driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@gmail.com");
+
+driver.findElement(By.xpath("//form/input[3]")).sendKeys("9864353253");
+
+driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+
+System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+
+driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+
+Thread.sleep(1000);
+
+driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+
+driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+
+driver.findElement(By.id("chkboxOne")).click();
+
+driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+
+}
+
+
+
+}
+        * */
+
     }
 }
